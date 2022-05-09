5953047cb64654bb6238ac8f0d4c7d62f9c5fa77
John Botero <John_Botero@epam.com>
2022-05-09 10:18:54 -0500

    Fix

--- task02-slice.go
+++ task02-slice.go
@@ -4,8 +4,8 @@ func reverse(input []int64) (result []int64) {
 	elements := len(input)
 	result = make([]int64, elements)
 
-	for i, j := 0, elements-1; i < j; i, j = i+1, j-1 {
-		result[j], result[i] = input[i], input[j]
+	for i := elements - 1; i >= 0; i = i - 1 {
+		result[elements-(i+1)] = input[i]
 	}
 
 	return
--- task02-slice_test.go
+++ task02-slice_test.go
@@ -5,9 +5,8 @@ import (
 )
 
 func TestReverse(t *testing.T) {
-	testingArray := []int64{41, 20, 89, 69}
-
-	want := []int64{69, 89, 20, 41}
+	testingArray := []int64{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	want := []int64{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
 	got := reverse(testingArray)
 
 	if !arrayEqualInt(want, got) {
